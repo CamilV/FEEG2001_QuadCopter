56be27beb9dfa2b3e913b6da40ecf042b680e0c4
CamilV <crv1g16@soton.ac.uk>
2018-02-08 15:36:49 +0000

    decoder code

---
+++ Code/Telemetry_Decoder/Telemetry_Decoder.ino
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+#include <rExcel.h>
+rExcel myExcel;
+SoftwareSerial BTSerial(9,8); //rx tx
+char str[9];
+int idx = 0;
+//char SpreadSheet = "Drone";
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+  myExcel.clearInput();
+  myExcel.writeIndexed("Drone", idx+1, 1, "Time");
+  myExcel.writeIndexed("Drone", idx+1, 2, "Altitude");
+  myExcel.writeIndexed("Drone", idx+1, 3, "Throttle");
+  myExcel.writeIndexed("Drone", idx+1, 4, "AutoPilot");
+  myExcel.writeIndexed("Drone", idx+1, 5, "Grabber");
+}
+
+void loop() {
+  int index = 0;
+  if(BTSerial.available()>0){
+    while(BTSerial.available()>0){
+    char c = BTSerial.read();
+    str[index] = c;
+    index++;
+    delay(10);
+    }
+    
+    int alt = 100 * ((int)str[0]-'0') + 10 * ((int)str[1]-'0') + ((int)str[2]-'0');
+    int th = 100 * ((int)str[3]-'0') + 10 * ((int)str[4]-'0') + ((int)str[5]-'0');
+    int AutoPilot = (int)str[6]-'0';
+    int Grabber = (int)str[7]-'0';
+    myExcel.writeIndexed("Drone", idx+2, 1, "%time%");
+    myExcel.writeIndexed("Drone", idx+2, 2, alt);
+    myExcel.writeIndexed("Drone", idx+2, 3, th);
+    myExcel.writeIndexed("Drone", idx+2, 4, AutoPilot);
+    myExcel.writeIndexed("Drone", idx+2, 5, Grabber);
+    idx++;
+    /*
+    Serial.print("Altitude: ");Serial.println(alt);
+    Serial.print("Throttle: ");Serial.println(th);
+    Serial.print("AutoPilot: ");Serial.println(AutoPilot);
+    Serial.print("Servo: ");Serial.println(Grabber);
+    */
+  }
+  delay(500);
+}
